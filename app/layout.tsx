0ae1a6cd5ab94316f630c9c910e5a0feae4d0221
startupcreed <startupcreed@gmail.com>
2026-04-25 07:46:28 +0530

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import WhatsAppFloat from '@/components/WhatsAppFloat'
-import GoogleTagManager from '@/components/GoogleTagManager'
 import MetaPixel from '@/components/MetaPixel'
 import { metadata } from '@/components/Seo'
 import { LeadPopupProvider } from '@/components/LeadPopupForm'
@@ -20,9 +19,21 @@ export default function RootLayout({
       <head>
         <link rel="canonical" href="https://keralatour.info/" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-53DHTWZL');`
+        }}
+      />
       </head>
       <body className='relative'>
-        <GoogleTagManager />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53DHTWZL"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <MetaPixel />
         <div className='circle-pink hidden lg:block'/>
         <div className='circle-yellow hidden xl:block'/>
